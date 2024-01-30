6b40ef7e2b5cd1c8dee692509edf26916ff26a2a
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-01-31 00:45:30 +0100

    Avoid segmentation fault when no PATH in envp

--- sources/get_path.c
+++ sources/get_path.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 15:10:56 by deydoux           #+#    #+#             */
-/*   Updated: 2024/01/30 18:16:27 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/01/31 00:42:44 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 char	**get_path(char **envp)
 {
-	while (ft_strncmp(*envp, "PATH=", 5))
+	while (*envp && ft_strncmp(*envp, "PATH=", 5))
 		envp++;
+	if (!*envp)
+		return (NULL);
 	return (ft_split(*envp + 5, ':'));
 }
