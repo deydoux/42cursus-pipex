82a145525e85837cb58f01d37ce601df3b8386a7
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-02-09 15:46:10 +0100

    Improve error handling

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 01:04:26 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/09 15:08:09 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/09 15:45:40 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_list	*cmds;
-	bool	exit_status;
+	bool	error;
 
-	if (argc < 4)
+	if (argc < 5)
 	{
 		ft_putstr_fd("pipex: Too few arguments\n", 2);
 		return (1);
 	}
 	cmds = NULL;
-	exit_status = parse_cmds(argc - 3, argv + 2, envp, &cmds);
+	error = parse_cmds(argc - 3, argv + 2, envp, &cmds);
 	ft_lstclear(&cmds, free_cmd);
-	return (exit_status);
+	if (error)
+		perror("pipex");
+	return (error);
 }
