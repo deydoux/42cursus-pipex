a02749444de48db50ebfc0fea313657349cd690d
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-02-11 12:29:13 +0100

    Improve pipex_test

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 01:04:26 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/10 15:19:59 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/11 12:19:54 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,36 @@
 static bool	pipex_test(t_list *cmds, char **envp)
 {
 	int		fd[2];
-	pid_t	pid1;
-	pid_t	pid2;
+	pid_t	pid[2];
 
 	if (pipe(fd) == -1)
 		return (true);
-	pid1 = fork();
-	if (pid1 == -1)
+	pid[0] = fork();
+	if (pid[0] == -1)
 		return (true);
-	if (!pid1)
+	if (!pid[0])
 	{
 		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
 		execve(((t_cmd *)cmds->content)->path, ((t_cmd *)cmds->content)->argv, envp);
 	}
-	cmds = cmds->next;
-	pid2 = fork();
-	if (pid2 == -1)
-		return (true);
-	if (!pid2)
+	else
 	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execve(((t_cmd *)cmds->content)->path, ((t_cmd *)cmds->content)->argv, envp);
+		cmds = cmds->next;
+		pid[1] = fork();
+		if (pid[1] == -1)
+			return (true);
+		if (!pid[1])
+		{
+			dup2(fd[1], STDOUT_FILENO);
+			execve(((t_cmd *)cmds->content)->path, ((t_cmd *)cmds->content)->argv, envp);
+		}
+		else
+		{
+			close(fd[0]);
+			close(fd[1]);
+			wait(NULL);
+		}
 	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 	return (false);
 }
 
