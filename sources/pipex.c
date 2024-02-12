45f80290b78be50cd7c0ca373bd39ecfd73488a1
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-02-12 16:12:05 +0100

    Add outfile

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 01:04:26 by deydoux           #+#    #+#             */
-/*   Updated: 2024/02/12 07:50:20 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/02/12 14:57:39 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ static bool	pipex_test(t_list *cmds, char **envp)
 {
 	int		fd[2];
 	pid_t	pid[2];
+	int		outfile;
 
+	ft_putnbr_fd(outfile_mode, 1);
+	outfile = open("outfile", outfile_flags, outfile_mode);
 	if (pipe(fd) == -1)
 		return (true);
 	pid[0] = fork();
@@ -24,9 +27,11 @@ static bool	pipex_test(t_list *cmds, char **envp)
 		return (true);
 	if (!pid[0])
 	{
-		dup2(fd[0], STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 		execve(((t_cmd *)cmds->content)->path, ((t_cmd *)cmds->content)->argv, envp);
-		exit(0);
+		exit(1);
 	}
 	cmds = cmds->next;
 	pid[1] = fork();
@@ -34,11 +39,17 @@ static bool	pipex_test(t_list *cmds, char **envp)
 		return (true);
 	if (!pid[1])
 	{
-		dup2(fd[1], STDOUT_FILENO);
+		dup2(fd[0], STDIN_FILENO);
+		dup2(outfile, STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 		execve(((t_cmd *)cmds->content)->path, ((t_cmd *)cmds->content)->argv, envp);
 		exit(0);
 	}
-	wait(NULL);
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], NULL, 0);
 	return (false);
 }
 
